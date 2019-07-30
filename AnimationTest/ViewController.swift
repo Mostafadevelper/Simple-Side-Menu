008e356b6bf958e4f5103648e69b933b8d954b13
Mostafadevelper <Mostafa.develper@gmail.com>
2019-07-30 18:23:18 +0200

    Initial Commit

---
+++ AnimationTest/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  AnimationTest
+//
+//  Created by Mostafa on 7/30/19.
+//  Copyright © 2019 pc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // My AutoLayout
+    
+    @IBOutlet weak var SideMenuLayout: NSLayoutConstraint!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func SideMenuButton(_ sender: UIButton) {
+        
+        // <=== Ternary Operator ===>
+        
+        SideMenuLayout.constant = (SideMenuLayout.constant == 0) ?  view.frame.size.width * 0.7 : 0
+        
+        UIView.animate(withDuration: 1) {
+            self.view.layoutIfNeeded()
+        }
+ 
+    }
+    
+}
+
